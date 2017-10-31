73206aeb50de5b524103b13bcb7a24e2b60f8bb8
Tri Ahmad Irfan <irfan@stoqo.com>
2017-10-31 16:30:48 +0700

    Implement transfer

--- server.py
+++ server.py
@@ -11,11 +11,14 @@ db = TinyDB('db.json')
 DB = Query()
 
 LIST_URL = 'http://152.118.31.2/list.php'
-
+MAX_TRANSFER_AMOUNT = 1000000000
 ZERO_QUORUM = 0
 HALF_QUORUM = 5
 FULL_QUORUM = 5
 
+# ======== HELPER METHODS ========
+
+# return IPs of neighbors corresponding to a list of IDs
 def get_neighbor_ips():
     neighbors = [
         '1406543574',
@@ -38,6 +41,86 @@ def get_neighbor_ips():
 
     return neighbor_ips
 
+
+# return IP address of first IP that has user_id as member
+# return -1 if can't get user_id on all nodes
+def pass_get_total_saldo(user_id):
+    neighbor_ips = get_neighbor_ips()
+    for neighbor_ip in neighbor_ips:
+        url = 'http://' + neighbor_ip + '/ewallet/getSaldo'
+        try:
+            response = requests.post(url, json={
+                'user_id': user_id
+            }, timeout=1)
+            response = response.json()
+            if response['nilai_saldo'] >= 0:
+                # call getTotalSaldo
+                next_url = 'http://' + neighbor_ip + '/ewallet/getTotalSaldo'
+                try:
+                    response = requests.post(next_url, json={
+                        'user_id': user_id
+                    }, timeout=2)
+                    response = response.json()
+                    print("[passGetTotalSaldo] Sucessfully getting saldo of: {}, from {}".format(response['nilai_saldo'], neighbor_ip))
+                    return response['nilai_saldo']
+                except Exception as e:
+                    print(e)
+                    print("[passGetTotalSaldo] Error getTotalSaldo from {}".format(neighbor_ip))
+        except Exception as e:
+            print(e)
+            print("[passGetTotalSaldo] Can't connect to: {}".format(url))
+
+    return -1
+
+
+# return -3 if can't connect to one of the host
+# return >=0 as the total saldo if successful
+def get_neighbors_total_saldo(user_id):
+    print("MASUK MASUK MASUK")
+    total_saldo = 0
+    neighbor_ips = get_neighbor_ips()
+    for neighbor_ip in neighbor_ips:
+        url = 'http://' + neighbor_ip + '/ewallet/getSaldo'
+        try:
+            response = requests.post(url, json={
+                'user_id': user_id
+            }, timeout=1)
+            response = response.json()
+            if response['nilai_saldo'] >= 0:
+                print("[getNeighborsSaldo] Succesfully adding: {}, from {}".format(response['nilai_saldo'], neighbor_ip), )
+                nilai_saldo = response['nilai_saldo']
+                total_saldo += nilai_saldo
+            else:
+                print("[getNeighborsSaldo] Failed getting saldo from {}".format(neighbor_ip))
+        except Exception as e:
+            print(e)
+            print("[getNeighborsSaldo] Can't connect to: {}".format(url))
+            return -3
+
+    return total_saldo
+
+# return the number of available neighbors
+def quorum_check():
+    neighbor_ips = get_neighbor_ips()
+
+    available = 0
+    for neighbor_ip in neighbor_ips:
+        url = 'http://' + neighbor_ip + '/ewallet/ping'
+        try:
+            status = requests.post(url, json={}, timeout=0.5)
+            status = status.json()
+            if status['pong'] == 1:
+                available += 1
+                print("url {} available".format(url))
+            else:
+                print("url {} not available".format(url))
+        except:
+            print("Can't connect to: {}".format(url))
+
+    return available
+
+
+# ======== ROUTING ========
 @app.route('/ewallet/ping', methods=['GET', 'POST'])
 def ping():
     if request.method == 'POST':
@@ -114,6 +197,43 @@ def get_saldo():
 
     return jsonify(response)
 
+@app.route('/ewallet/transfer', methods=['POST'])
+def trasfer():
+    if request.method == 'POST':
+        quorum_result = quorum_check()
+        if quorum_result >= HALF_QUORUM:
+            req = request.get_json()
+            user_id = req.get('user_id', None)
+            nilai = req.get('nilai', None)
+
+            if user_id and nilai:
+                result = db.search(DB.user_id == user_id)
+                if len(result) > 0:
+                    nilai = int(nilai)
+                    if(nilai >= 0 and nilai <= MAX_TRANSFER_AMOUNT):
+                        # do transfer
+                        status_transfer = 1
+                        saldo_awal = result[0]['nilai_saldo']
+                        db.update({
+                            'nilai_saldo' : saldo_awal + nilai
+                        }, DB.user_id == user_id)
+                    else:
+                        status_transfer = -5
+                else:
+                    status_transfer = -4
+            else:
+                status_transfer = -99
+        else:
+            status_transfer = -2
+    else:
+        status_transfer = -99
+
+    response = {
+        'status_transfer': status_transfer
+    }
+
+    return jsonify(response)
+
 @app.route('/ewallet/getTotalSaldo', methods=['POST'])
 def get_total_saldo():
     print("Starting method")
@@ -150,99 +270,3 @@ def get_total_saldo():
     }
 
     return jsonify(response)
-
-# return IP address of first IP that has user_id as member
-# return -1 if can't get user_id on all nodes
-def pass_get_total_saldo(user_id):
-    neighbor_ips = get_neighbor_ips()
-    for neighbor_ip in neighbor_ips:
-        url = 'http://' + neighbor_ip + '/ewallet/getSaldo'
-        try:
-            response = requests.post(url, json={
-                'user_id': user_id
-            }, timeout=1)
-            response = response.json()
-            if response['nilai_saldo'] >= 0:
-                # call getTotalSaldo
-                next_url = 'http://' + neighbor_ip + '/ewallet/getTotalSaldo'
-                try:
-                    response = requests.post(next_url, json={
-                        'user_id': user_id
-                    }, timeout=2)
-                    response = response.json()
-                    print("[passGetTotalSaldo] Sucessfully getting saldo of: {}, from {}".format(response['nilai_saldo'], neighbor_ip))
-                    return response['nilai_saldo']
-                except Exception as e:
-                    print(e)
-                    print("[passGetTotalSaldo] Error getTotalSaldo from {}".format(neighbor_ip))
-        except Exception as e:
-            print(e)
-            print("[passGetTotalSaldo] Can't connect to: {}".format(url))
-
-    return -1
-
-
-# return -3 if can't connect to one of the host
-# return >=0 as the total saldo if successful
-def get_neighbors_total_saldo(user_id):
-    print("MASUK MASUK MASUK")
-    total_saldo = 0
-    neighbor_ips = get_neighbor_ips()
-    for neighbor_ip in neighbor_ips:
-        url = 'http://' + neighbor_ip + '/ewallet/getSaldo'
-        try:
-            response = requests.post(url, json={
-                'user_id': user_id
-            }, timeout=1)
-            response = response.json()
-            if response['nilai_saldo'] >= 0:
-                print("[getNeighborsSaldo] Succesfully adding: {}, from {}".format(response['nilai_saldo'], neighbor_ip), )
-                nilai_saldo = response['nilai_saldo']
-                total_saldo += nilai_saldo
-            else:
-                print("[getNeighborsSaldo] Failed getting saldo from {}".format(neighbor_ip))
-                return -3
-        except Exception as e:
-            print(e)
-            print("[getNeighborsSaldo] Can't connect to: {}".format(url))
-            return -3
-
-    return total_saldo
-
-def quorum_check():
-    neighbors = [
-        '1406543574',
-        '1406579100',
-        '1406543725',
-        '1406527620',
-        '1406527513',
-        '1406527532',
-        '1406543624'
-    ]
-
-    neighbor_ips = []
-
-    print("[quorumCheck] getting list URL")
-    ips = requests.get(LIST_URL, timeout=1).json()
-    print("[quorumCheck] finished getting list URL")
-
-    for ip in ips:
-        for neighbor in neighbors:
-            if ip['npm'] == neighbor:
-                neighbor_ips.append(ip['ip'])
-
-    available = 0
-    for neighbor_ip in neighbor_ips:
-        url = 'http://' + neighbor_ip + '/ewallet/ping'
-        try:
-            status = requests.post(url, json={}, timeout=0.5)
-            status = status.json()
-            if status['pong'] == 1:
-                available += 1
-                print("url {} available".format(url))
-            else:
-                print("url {} not available".format(url))
-        except:
-            print("Can't connect to: {}".format(url))
-
-    return available
